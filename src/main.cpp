c5b62e59a1a52faeba30eef88af1dd75e23a6f37
Kent Francis Genilo <kegenilo@up.edu.ph>
2025-11-18 22:30:20 +0800

    remove unnecessary comment

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,3 @@ int main() {
     game.ChangeState(std::make_unique<StartState>());
     game.Run();
 }
-
-// case GameState::GameOver: 
-    //             // gameOverScreen.Draw();
-    //             // gameOverScreen.SetWaveReached(currentWave);
-    //             // gameOverScreen.Update();
-    //             // if (gameOverScreen.IsTryAgainPressed()) gameState = GameState::GameOngoing;
-    //             // if (gameOverScreen.IsExitPressed()) {
-    //             //     CloseWindow();
-    //             //     return 0;
-    //             // }
-    //             break;
